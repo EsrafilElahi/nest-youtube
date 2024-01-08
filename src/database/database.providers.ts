36981c3cadda7cfc3ab24ddbec8a2c80f21a26d1
esrafil elahi <elahi.esrafil@okala.com>
2024-01-08 17:54:05 +0330

    change postgres to mysql

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,22 +1,38 @@
 /* eslint-disable prettier/prettier */
-import { DataSource } from 'typeorm';
+// import { DataSource } from 'typeorm';
 
-export const databaseProviders = [
-  {
-    provide: 'DATA_SOURCE',
-    useFactory: async () => {
-      const dataSource = new DataSource({
-        type: 'mysql',
-        host: String(process.env.MYSQL_HOST),
-        port: Number(process.env.MYSQL_PORT),
-        username: String(process.env.MYSQL_USERNAME),
-        password: String(process.env.MYSQL_PASSWORD),
-        database: String(process.env.MYSQL_DATABASE),
-        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-        synchronize: true,
-      });
+// export const databaseProviders = [
+//   {
+//     provide: 'DATA_SOURCE',
+//     useFactory: async () => {
+//       const dataSource = new DataSource({
+//         type: 'mysql',
+//         host: String(process.env.MYSQL_HOST),
+//         port: Number(process.env.MYSQL_PORT),
+//         username: String(process.env.MYSQL_USERNAME),
+//         password: String(process.env.MYSQL_PASSWORD),
+//         database: String(process.env.MYSQL_DATABASE),
+//         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+//         synchronize: true,
+//       });
 
-      return dataSource.initialize();
-    },
-  },
-];
+//       return dataSource.initialize();
+//     },
+//   },
+// ];
+
+// database.config.ts
+
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const databaseConfig = (configService: ConfigService): TypeOrmModuleOptions => ({
+  type: 'mysql',
+  host: configService.get('DB_HOST'),
+  port: configService.get('DB_PORT'),
+  username: configService.get('DB_USERNAME'),
+  password: configService.get('DB_PASSWORD'),
+  database: configService.get('DB_DATABASE'),
+  synchronize: true,
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+});
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  const FRONTEND_URL = configService.get<string>('FRONTEND_URL');
+  const PORT = configService.get<number>('PORT');
+
+  app.setGlobalPrefix('api');
+
+  app.enableCors({
+    origin: FRONTEND_URL,
+    allowedHeaders: 'Content-Type, Authorization',
+    methods: 'GET, PUT, POST, DELETE',
+    credentials: true,
+  });
+
+  await app.listen(PORT);
 }
 bootstrap();
