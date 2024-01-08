4fd8a7d332f7a374557d1d69f5141fdfe3e1b489
esrafil elahi <elahi.esrafil@okala.com>
2024-01-08 18:16:34 +0330

    delete extra config directory

--- src/config/config.ts
+++
@@ -1,7 +0,0 @@
-export default () => ({
-  port: parseInt(process.env.PORT, 10) || 5000,
-  database: {
-    host: process.env.MYSQL_HOST,
-    port: parseInt(process.env.MYSQL_PORT, 10) || 5432,
-  },
-});
