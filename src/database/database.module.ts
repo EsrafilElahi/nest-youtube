5774ac44a736a2aa8c6fcc919679d582cc142f45
esrafilelahi <essinho1121@gmail.com>
2024-01-14 01:11:36 +0330

    add global config guid

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,9 +1,10 @@
 /* eslint-disable prettier/prettier */
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { databaseConfig } from './database.providers';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+@Global() // make global, not need import
 @Module({
   imports: [
     ConfigModule.forRoot(),
