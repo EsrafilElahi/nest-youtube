a872a306be0ee693592d701a80a18f07e822717c
esrafilelahi <essinho1121@gmail.com>
2024-01-18 22:34:52 +0330

    global explanation

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,13 +1,14 @@
-
 import { Global, Module } from '@nestjs/common';
 import { databaseConfig } from './database.providers';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+// t indicates that the module and its provided services will be instantiated only once and shared globally across the entire application. This can be useful when you want to create singleton services that should be reused throughout the entire application
+// when this module is imported into other modules, the services and components it provides will be shared across the entire application. if we don't put this @Global(), in another modules must be imported manually in import[]
 @Global() // make global, not need import
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    // ConfigModule.forRoot(),
     // for all pages applied
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
