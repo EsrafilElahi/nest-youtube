500c608b39cb7badfa33749d82ddf06eb0fe9d1c
esrafilelahi <essinho1121@gmail.com>
2024-01-25 21:32:46 +0330

    add ExtractUser decorator

---
+++ src/common/decorators/ExtractUser.ts
@@ -0,0 +1,8 @@
+import { ExecutionContext, createParamDecorator } from '@nestjs/common';
+// import { AuthEntity } from 'src/entities/auth.entity';
+
+export const ExtractUser = createParamDecorator((data, ctx: ExecutionContext) => {
+  const req = ctx.switchToHttp().getRequest();
+  //   return req.user;
+  console.log('reqqqqqqq :', req);
+});
--- src/common/decorators/extractuser.ts
+++
@@ -1,1 +0,0 @@
-// extract user decorator
