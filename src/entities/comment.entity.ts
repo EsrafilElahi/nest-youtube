18f8375b58aa41bba28561291b21af89566e6d91
esrafilelahi <essinho1121@gmail.com>
2024-01-26 14:18:26 +0330

    commnet

--- src/entities/comment.entity.ts
+++ src/entities/comment.entity.ts
@@ -9,22 +9,7 @@ export class CommentEntity extends AbstractEntity {
   auth: AuthEntity;
 
   @Column()
-  @IsString({ message: 'title should be a string!' })
-  @IsNotEmpty({ message: 'title should not be empty!' })
-  title: string;
-
-  @Column()
-  @IsString({ message: 'description should be a string!' })
+  @IsString({ message: 'text should be a string!' })
   @IsOptional()
-  description: string;
-
-  @Column()
-  @IsString({ message: 'url should be a string!' })
-  @IsOptional()
-  url: string;
-
-  @Column()
-  @IsNumber({}, { message: 'url should be a number!' })
-  @IsOptional()
-  views: number;
+  text: string;
 }
