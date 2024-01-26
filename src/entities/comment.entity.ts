a2e00f838ff3d560d171ae90d07eedf832066ba1
esrafilelahi <essinho1121@gmail.com>
2024-01-26 14:38:43 +0330

    comment

--- src/entities/comment.entity.ts
+++ src/entities/comment.entity.ts
@@ -25,3 +25,11 @@ export class CommentEntity extends AbstractEntity {
   @ManyToOne(() => CommentEntity, (comment) => comment.replies, { eager: true, cascade: true })
   parentComment: CommentEntity;
 }
+
+// eager: true (Eager Loading):
+// When eager: true is set on a relationship, it means that the related entity will be loaded along with the main entity when querying the database.
+// In your case, when you load a CommentEntity from the database, the associated AuthEntity, replies, and parentComment will be loaded eagerly, meaning all the related entities will be fetched in a single query.
+
+// lazy: true (Lazy Loading):
+// When lazy: true is set on a relationship, it means that the related entity will be loaded only when explicitly requested.
+// In your case, if you access the replies or parentComment property of a CommentEntity instance, the related entities will be loaded at that point, triggering additional queries to the database.
