937ef6e088885dd83592655b3aaa3810540715e9
esrafilelahi <essinho1121@gmail.com>
2024-01-26 14:37:46 +0330

    comment

--- src/entities/comment.entity.ts
+++ src/entities/comment.entity.ts
@@ -15,10 +15,13 @@ export class CommentEntity extends AbstractEntity {
 
   @Column()
   @JoinColumn()
-  @OneToMany(() => CommentEntity, (comment) => comment.parentComment, { eager: true, lazy: true, cascade: true })
+  @OneToMany(() => CommentEntity, (comment) => comment.parentComment, { eager: true, cascade: true })
   replies: CommentEntity[];
 
+  // Eager loading is beneficial when you know you'll always need the related data, as it reduces the number of queries by fetching everything in one go.
+  // Lazy loading is useful when you want to avoid fetching unnecessary data and only load related entities when needed.
+
   @Column()
-  @ManyToOne(() => CommentEntity, (comment) => comment.replies, { eager: true, lazy: true, cascade: true })
+  @ManyToOne(() => CommentEntity, (comment) => comment.replies, { eager: true, cascade: true })
   parentComment: CommentEntity;
 }
